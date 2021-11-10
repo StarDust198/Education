50d4a8c585ae2cf4d5d780d9b407fafce7293050
StarDust85z <roadtomars2030@gmail.com>
2021-11-10 19:32:19 +0300

    forms load/success/fail messages

--- dist/script.js
+++ dist/script.js
@@ -2394,6 +2394,14 @@ __webpack_require__.r(__webpack_exports__);
 class Forms {
   constructor(forms) {
     this.forms = document.querySelectorAll(forms);
+    this.message = {
+      loading: 'Loading...',
+      success: 'Success! Thank you!',
+      failure: 'Something went wrong..',
+      spinner: 'assets/img/spinner.gif',
+      ok: 'assets/img/ok.png',
+      fail: 'assets/img/fail.png'
+    };
   }
 
   checkEmailInput(input) {
@@ -2412,11 +2420,34 @@ class Forms {
       this.checkEmailInput(form.querySelector('input[type="email"]'));
       form.addEventListener('submit', e => {
         e.preventDefault();
+        let statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        form.appendChild(statusMessage);
+        let statusImg = document.createElement('img');
+        statusImg.src = this.message.spinner;
+        statusImg.classList.add('animated', 'fadeInUp');
+        statusMessage.appendChild(statusImg);
+        let textMessage = document.createElement('div');
+        textMessage.textContent = this.message.loading;
+        statusMessage.appendChild(textMessage);
         const formData = new FormData(form);
         const obj = {};
         formData.forEach((value, key) => obj[key] = value);
         const json = JSON.stringify(obj);
-        Object(_services_requests__WEBPACK_IMPORTED_MODULE_1__["postData"])('https://jsonplaceholder.typicode.com/posts', json).then(res => console.log(res)).catch(e => console.error(e));
+        Object(_services_requests__WEBPACK_IMPORTED_MODULE_1__["postData"])('https://jsonplaceholder.typicode.com/posts', json).then(res => {
+          console.log(res);
+          statusImg.setAttribute('src', this.message.ok);
+          textMessage.textContent = this.message.success;
+        }).catch(e => {
+          console.error(e);
+          statusImg.setAttribute('src', this.message.fail);
+          textMessage.textContent = this.message.failure;
+        }).finally(() => {
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 3000);
+        });
       });
     });
   }
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -4,6 +4,14 @@ import mask from "../services/mask";
 export default class Forms {
     constructor (forms) {
         this.forms = document.querySelectorAll(forms);
+        this.message = {
+            loading: 'Loading...',
+            success: 'Success! Thank you!',
+            failure: 'Something went wrong..',
+            spinner: 'assets/img/spinner.gif',
+            ok: 'assets/img/ok.png',
+            fail: 'assets/img/fail.png'
+        };
     }
 
     checkEmailInput(input) {
@@ -16,14 +24,28 @@ export default class Forms {
         mask('#phone');
     }
 
+
     bindForms() {
         this.checkPhoneInput();
 
         this.forms.forEach(form => {
-            this.checkEmailInput(form.querySelector('input[type="email"]'));            
+            this.checkEmailInput(form.querySelector('input[type="email"]'));
 
             form.addEventListener('submit', (e) => {
                 e.preventDefault();
+
+                let statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                form.appendChild(statusMessage);    
+
+                let statusImg = document.createElement('img');
+                statusImg.src = this.message.spinner;
+                statusImg.classList.add('animated', 'fadeInUp');
+                statusMessage.appendChild(statusImg);
+
+                let textMessage = document.createElement('div');
+                textMessage.textContent = this.message.loading;
+                statusMessage.appendChild(textMessage);   
 
                 const formData = new FormData(form);
                 const obj = {};
@@ -31,8 +53,22 @@ export default class Forms {
                 const json = JSON.stringify(obj);
 
                 postData('https://jsonplaceholder.typicode.com/posts', json)
-                    .then(res => console.log(res))
-                    .catch((e) => console.error(e));
+                    .then(res => {
+                        console.log(res);
+                        statusImg.setAttribute('src', this.message.ok);
+                        textMessage.textContent = this.message.success;
+                    })
+                    .catch((e) => {
+                        console.error(e);
+                        statusImg.setAttribute('src', this.message.fail);
+                        textMessage.textContent = this.message.failure;
+                    })
+                    .finally(() => {
+                        form.reset();
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 3000);
+                    });
             });
         });
     }
